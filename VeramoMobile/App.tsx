cb1a6964e0e6ffc322e319091435178cb1f94cc7
Taeyong Hwang <xodyd15@naver.com>
2021-10-05 16:19:06 +0900

    add

--- VeramoMobile/App.tsx
+++ VeramoMobile/App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import {SafeAreaView, ScrollView, View, Text, Button} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native'
 
 // Import agent from setup
-import {agent} from './src/veramo/setup'
+import { agent } from './src/veramo/setup'
 
 interface Identifier {
     did: string
@@ -14,7 +14,6 @@ const App = () => {
     // Add the new identifier to state
     const createIdentifier = async () => {
         const _id = await agent.didManagerCreate()
-
         setIdentifiers((s) => s.concat([_id]))
     }
 
@@ -26,20 +25,6 @@ const App = () => {
 
             // Inspect the id object in your debug tool
             console.log('_ids:', _ids)
-
-            //resolve
-            const didDoc = await agent.resolveDid({
-                didUrl: 'did:ethr:rinkeby:0x0327bfaeaf5ec200d82d41c551671450c11d9c1294407c70f207ed114db0ea9452',
-            })
-            console.log('resolve')
-            console.log(JSON.stringify(didDoc, null, 2))
-
-            //selective disclosure
-            const msg = await agent.handleMessage({
-                raw: 'https://id.uport.me/req/eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE2MzIxMjc5MDYsImV4cCI6MTYzMjEyODUwNiwicGVybWlzc2lvbnMiOlsibm90aWZpY2F0aW9ucyJdLCJjYWxsYmFjayI6Imh0dHBzOi8vN2E5OS0xNzUtMjIzLTQ5LTE5OC5uZ3Jvay5pby91cG9ydHMvd2ViaG9va3MvaXNzdWFuY2UiLCJ0eXBlIjoic2hhcmVSZXEiLCJpc3MiOiJkaWQ6ZXRocjoweDFhMmM4ZTQwN2JhNjc4OTdiMzkzMzdkZmM2OTJhZDIxNjMzYTM4OTIifQ.kGVUCMGCypNT1VIZSO6Xvrgq57iEeett1Eabl2REaTQAHuYzTbT5Tm8wqjmpXGGF6g9tvE8SNqklajVIHTZsCgE?callback_type=post',
-            });
-
-            console.log(msg)
         }
 
         getIdentifiers()
@@ -48,9 +33,9 @@ const App = () => {
     return (
         <SafeAreaView>
             <ScrollView>
-                <View style={{padding: 20}}>
-                    <Text style={{fontSize: 30, fontWeight: 'bold'}}>Identifiers</Text>
-                    <View style={{marginBottom: 50, marginTop: 20}}>
+                <View style={{ padding: 20 }}>
+                    <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Identifiers</Text>
+                    <View style={{ marginBottom: 50, marginTop: 20 }}>
                         {identifiers && identifiers.length > 0 ? (
                             identifiers.map((id: Identifier) => (
                                 <View key={id.did}>
@@ -61,7 +46,7 @@ const App = () => {
                             <Text>No identifiers created yet</Text>
                         )}
                     </View>
-                    <Button onPress={() => createIdentifier()} title={'Create Identifier'}/>
+                    <Button onPress={() => createIdentifier()} title={'Create Identifier'} />
                 </View>
             </ScrollView>
         </SafeAreaView>
---
+++ veramoMobile3/src/veramo/setup.ts
@@ -0,0 +1,122 @@
+// Core interfaces
+import {
+    createAgent,
+    IDIDManager,
+    IResolver,
+    IDataStore,
+    IKeyManager,
+    IMessageHandler,
+    IEventListener
+} from '@veramo/core'
+
+// Core identity manager plugin
+import { DIDManager } from '@veramo/did-manager'
+
+// Ethr did identity provider
+import { EthrDIDProvider } from '@veramo/did-provider-ethr'
+
+// Web did identity provider
+import { WebDIDProvider } from '@veramo/did-provider-web'
+
+// Core key manager plugin
+import {AbstractKeyStore, KeyManager} from '@veramo/key-manager'
+
+// Custom key management system for RN
+import {KeyManagementSystem, SecretBox} from '@veramo/kms-local'
+
+// Custom resolvers
+import { DIDResolverPlugin } from '@veramo/did-resolver'
+import { Resolver } from 'did-resolver'
+import { getResolver as ethrDidResolver } from 'ethr-did-resolver'
+import { getResolver as webDidResolver } from 'web-did-resolver'
+
+// Storage plugin using TypeOrm
+import {
+    Entities,
+    KeyStore,
+    DIDStore,
+    IDataStoreORM,
+    PrivateKeyStore,
+    DataStore,
+    DataStoreORM,
+    migrations
+} from '@veramo/data-store'
+
+// TypeORM is installed with `@veramo/data-store`
+import { createConnection } from 'typeorm'
+import {MessageHandler} from "@veramo/message-handler";
+import {DIDComm, DIDCommHttpTransport, DIDCommMessageHandler, IDIDComm} from "@veramo/did-comm";
+import {JwtMessageHandler} from "@veramo/did-jwt";
+import {CredentialIssuer, ICredentialIssuer, W3cMessageHandler} from "@veramo/credential-w3c";
+import {ISelectiveDisclosure, SdrMessageHandler, SelectiveDisclosure} from "@veramo/selective-disclosure";
+
+// This will be the name for the local sqlite database for demo purposes
+const DATABASE_FILE = 'database.sqlite'
+
+// You will need to get a project ID from infura https://www.infura.io
+const INFURA_PROJECT_ID = '2b64268fa545491f90167f46698876db'
+
+const secretKey = '29739248cad1bd1a0fc4d9b75cd4d2990de535baf5caadfdf8d8f86664aa830c'
+
+const dbConnection = createConnection({
+    name: 'test',
+    type: 'sqlite',
+    database: DATABASE_FILE,
+    synchronize: false,
+    logging: ['error', 'info', 'warn'],
+    migrations: migrations,
+    migrationsRun: true,
+    entities: Entities,
+})
+
+const DIDCommEventSniffer: IEventListener = {
+    eventTypes: ['DIDCommV2Message-sent', 'DIDCommV2Message-received'],
+    onEvent: async(req) => {},
+
+}
+
+export const agent = createAgent<IDIDManager & IKeyManager & IDataStore & IDataStoreORM & IResolver & IMessageHandler & IDIDComm & ICredentialIssuer & ISelectiveDisclosure>({
+    plugins: [
+        new KeyManager({
+            store: new KeyStore(dbConnection),
+            kms: {
+                local: new KeyManagementSystem(new PrivateKeyStore(dbConnection, new SecretBox(secretKey))),
+            },
+        }),
+        new DIDManager({
+            store: new DIDStore(dbConnection),
+            defaultProvider: 'did:ethr:rinkeby',
+            providers: {
+                'did:ethr:rinkeby': new EthrDIDProvider({
+                    defaultKms: 'local',
+                    network: 'rinkeby',
+                    rpcUrl: 'https://rinkeby.infura.io/v3/' + INFURA_PROJECT_ID,
+                    gas: 1000001,
+                    ttl: 60 * 60 * 24 * 30 * 12 + 1,
+                }),
+                'did:web': new WebDIDProvider({
+                    defaultKms: 'local',
+                }),
+            },
+        }),
+        new DIDResolverPlugin({
+            resolver: new Resolver({
+                ...ethrDidResolver({ infuraProjectId: INFURA_PROJECT_ID }),
+                ...webDidResolver(),
+            }),
+        }),
+        new DataStore(dbConnection),
+        new DataStoreORM(dbConnection),
+        new MessageHandler({
+            messageHandlers: [
+                new DIDCommMessageHandler(),
+                new JwtMessageHandler(),
+                new W3cMessageHandler(),
+                new SdrMessageHandler(),
+            ],
+        }),
+        new DIDComm([new DIDCommHttpTransport()]),
+        new CredentialIssuer(),
+        new SelectiveDisclosure(),
+    ],
+})
