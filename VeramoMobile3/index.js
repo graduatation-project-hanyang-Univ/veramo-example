265c2400d5e5cb7606c35e768417fa42576c183d
황태용 <xodyd15@naver.com>
2021-10-08 09:51:46 +0900

    veramo3

---
+++ VeramoMobile3/index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import TextEncoder from 'text-encoding-polyfill';
+import './shim'
+import "react-native-get-random-values"
+import "@ethersproject/shims"
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
